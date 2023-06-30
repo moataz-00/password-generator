0386ad28fb3b25fce72a0fa269e858ad1ddf1adf
moataz-00 <tahamoataz5@gmail.com>
2023-06-30 18:43:37 +0200

    first1 commit

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 let box = document.getElementById("pass");
-let lenght =12;
-let co= document.getElementById('copy');
+let lenght = 12;
+let co = document.getElementById("copy");
 console.log(co);
 
-let uppercase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowercase="abcdefghijklmnopqrstuvwxyz";
-let number="0123456789";
-let symbol="@#$%^&*()_+~|}{[]<>/-=";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let number = "0123456789";
+let symbol = "@#$%^&*()_+~|}{[]<>/-=";
 
-let allchars= uppercase + lowercase+ number + symbol;
+let allchars = uppercase + lowercase + number + symbol;
 
-function createpassword(){
-    let password ="";
-    password +=uppercase[Math.floor(Math.random() * uppercase.length)];
-    password +=lowercase[Math.floor(Math.random() * lowercase.length)];
-    password +=number[Math.floor(Math.random() * number.length)];
-    password +=symbol[Math.floor(Math.random() * symbol.length)];
-    
-    while(lenght>password.length){
-        password +=allchars[Math.floor(Math.random() * allchars.length)];
-    }
-    
-    box.value=password;
+function createpassword() {
+  let password = "";
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
 
+  while (lenght > password.length) {
+    password += allchars[Math.floor(Math.random() * allchars.length)];
+  }
+
+  co.innerHTML = "<i style=' font-size: 30px;cursor: pointer;' class='fa-solid fa-copy'></i>";
+
+  box.value = password;
 }
 
-function copy(){
-    box.select();
-    document.execCommand('copy');
-    co.innerHTML="<i style=' font-size: 30px;' class='fa-solid fa-check'></i>"
-   
+function copy() {
+  box.select();
+  document.execCommand("copy");
+  co.innerHTML = "<i style=' font-size: 30px;' class='fa-solid fa-check'></i>";
 }
+
+AOS.init();
